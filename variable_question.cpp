27ee13a96e2e9d67f54000ad9a227bc379544609
Harsh Sharma xyz <hs5586528@gmail.com>
2024-08-23 23:12:46 +0530

    variable question

---
+++ variable_question.cpp
@@ -0,0 +1,22 @@
+/*Write a program to declare and initialize following variables:
+
+int roll_number = 21;
+float total_marks = 498;
+double temprature = 28.7;
+Print the value of these variables on console.*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare and initialize variables
+    int roll_number = 21;
+    float total_marks = 498.0;
+    double temperature = 28.7;
+
+    // Print the values of the variables
+    cout << "Roll Number: " << roll_number << endl;
+    cout << "Total Marks: " << total_marks << endl;
+    cout << "Temperature: " << temperature << endl;
+
+    return 0;
+}
