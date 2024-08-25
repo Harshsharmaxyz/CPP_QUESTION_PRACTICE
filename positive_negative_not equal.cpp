159c92e21794d3452b27228e6eaa95375614eb33
Harsh Sharma xyz <hs5586528@gmail.com>
2024-08-25 13:02:45 +0530

    if_else

---
+++ positive_negative_not equal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+
+    cout << "Enter a number" << "\n";
+    cin >> num;
+
+    if( num >= 0)
+    {
+        cout << "positive" << "\n";
+        
+    }
+    else{
+        cout << "negative" << "\n";
+    }
+}
